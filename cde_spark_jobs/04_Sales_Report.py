3f865f76cb7be5d48a7900d893c23df8468b93b9
pdefusco <pauldefusco@gmail.com>
2023-05-17 09:33:13 -0700

    modified script 4 to read username correctly

--- cde_spark_jobs/04_Sales_Report.py
+++ cde_spark_jobs/04_Sales_Report.py
@@ -107,7 +107,7 @@ print(testdf.dtypes)
 print("-------")
 
 print("REPLACE PARTITION FIELD MONTH WITH FIELD DAY:")
-print("ALTER TABLE spark_catalog.CDE_WORKSHOP.CAR_SALES_{} REPLACE PARTITION FIELD MONTH WITH DAY".format(username)))
+print("ALTER TABLE spark_catalog.CDE_WORKSHOP.CAR_SALES_{} REPLACE PARTITION FIELD MONTH WITH DAY".format(username))
 spark.sql("ALTER TABLE spark_catalog.CDE_WORKSHOP.CAR_SALES_{} REPLACE PARTITION FIELD month WITH day".format(username))
 #spark.sql("ALTER TABLE prod.db.sample ADD PARTITION FIELD month")
 
